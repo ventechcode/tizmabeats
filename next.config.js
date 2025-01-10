7f714e8c1ba101a2e70be10a2897368ec9c27c06
lukas47 <lukas47@zedat.fu-berlin.de>
2025-01-10 18:23:29 +0100

    adjusted config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'Cross-Origin-Opener-Policy',
-              value: 'same-origin',
-            },
-            {
-              key: 'Cross-Origin-Embedder-Policy',
-              value: 'require-corp',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  // Use CommonJS export
-  module.exports = nextConfig;
+const nextConfig = {};
+
+// Use CommonJS export
+module.exports = nextConfig;
