dc0aad5563efcf83aaa6ef2c2eacd3759001a76b
lukas47 <lukas47@zedat.fu-berlin.de>
2025-01-12 16:08:21 +0100

    next-auth basic setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { hashPassword } from "@/utils/crypto";
+export { auth as middleware } from "@/auth"
 
-export async function middleware(req: NextRequest) {
-    if (await isAuthenticated(req) === false) {
-        return new NextResponse("Unauthroized", { status: 401 , headers: { "WWW-Authenticate": "Basic" }});
-    } 
-}
+// export async function middleware(req: NextRequest) {
+//     if (await isAuthenticated(req) === false) {
+//         return new NextResponse("Unauthroized", { status: 401 , headers: { "WWW-Authenticate": "Basic" }});
+//     } 
+// }
 
-async function checkPassword(password: string) {
-    const hashedPassword = await hashPassword(password);
-    return process.env.ADMIN_PASSWORD === hashedPassword;
-}
+// async function checkPassword(password: string) {
+//     const hashedPassword = await hashPassword(password);
+//     return process.env.ADMIN_PASSWORD === hashedPassword;
+// }
 
-async function isAuthenticated(req: NextRequest) {
-    const authHeader = req.headers.get("Authorization") || req.headers.get("authorization");
-    if (!authHeader) return false;
+// async function isAuthenticated(req: NextRequest) {
+//     const authHeader = req.headers.get("Authorization") || req.headers.get("authorization");
+//     if (!authHeader) return false;
 
-    const base64Credentials = authHeader.split(" ")[1];
-    const credentials = Buffer.from(base64Credentials, "base64").toString("ascii");
-    const [username, password] = credentials.split(":");
-    return username === "admin" && (await checkPassword(password));
-}
+//     const base64Credentials = authHeader.split(" ")[1];
+//     const credentials = Buffer.from(base64Credentials, "base64").toString("ascii");
+//     const [username, password] = credentials.split(":");
+//     return username === "admin" && (await checkPassword(password));
+// }
 
-export const config = {
-    matcher: "/admin/:path*",
-}
+// export const config = {
+//     matcher: "/admin/:path*",
+// }
